9aee12d53eb5cb63acffee8087b6c465ab3093a2
Siddhartha Pal <115708913+Jkliop09@users.noreply.github.com>
2023-04-21 19:58:27 +0530

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head({ title, subtitle }: { title: string; subtitle: string }) {
   // Fallback tagline
-  title ??= "Share Environment Variables Securely";
-  subtitle ??= "EnvShare";
+  title ??= "Share Environment Variables Securely using hexor With your team";
+  subtitle ??= "hexor";
 
   const baseUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "http://localhost:3000";
 
